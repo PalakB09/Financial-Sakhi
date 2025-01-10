f495c9109e033f1cb903868c2f159d114389742e
Rishita77 <rishita.m8@gmail.com>
2025-01-10 21:26:47 +0530

    Game added

---
+++ Finance-Game/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import PaperTrader from './components/PaperTrader';
+
+import Chat from './components/Chat';
+import Roadmap from './components/Roadmap';
+
+const App: React.FC = () => {
+  return (
+    
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/roadmap" replace />} />
+           <Route path="/papertrade" element={<PaperTrader />} />
+
+           <Route path="/chat" element={<Chat />} />
+           <Route path="/roadmap" element={<Roadmap />} />
+           
+        </Routes>
+      </Router>
+
+  );
+};
+
+export default App;
